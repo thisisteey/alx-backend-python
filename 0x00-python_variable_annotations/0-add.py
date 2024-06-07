b5890cc93f4bbdd2d50d535871d526d5b1684e98
thisisteey <dadataiwo82@yahoo.com>
2024-06-07 23:41:19 +0000

    Python prohect that writes a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module defined for type-annoted function"""
+
+
+def add(a: float, b: float) -> float:
+    """Type-Annoted function that adds two floating numbers"""
+    return a + b
