d1902788b86d7191bf1d78901f8caa4b71431b1b
thisisteey <dadataiwo82@yahoo.com>
2024-06-10 14:20:32 +0000

    Python project that creates a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module defined for function measure_time"""
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Calculates the average runtime of wait_n function for a num of execs"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    return total_time / n
