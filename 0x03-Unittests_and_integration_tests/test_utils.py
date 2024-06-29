86be263e738cfb89107761e0fc2dc54996c4ff19
thisisteey <dadataiwo82@yahoo.com>
2024-06-29 17:07:54 +0000

    Feature: Add unit tests for utils.access_nested_map with parameterized inputs

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for testing the functions in the utils file"""
+import unittest
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Unittest for access_nested_map class/function"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
+        """Tests for function output: access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
