f7e5d3c81045510720fb4a89899164ddb65d7456
thisisteey <dadataiwo82@yahoo.com>
2024-06-29 18:59:42 +0000

    Feature: Add unit test for GithubOrgClient.public_repos with mocked get_json and _public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,3 +30,52 @@ class TestGithubOrgClient(unittest.TestCase):
             }
             self.assertEqual(GithubOrgClient("google")._public_repos_url,
                              "https://api.github.com/users/google/repos")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mockget_json: MagicMock) -> None:
+        """Test the public_repos method of the GithubOrgClient class"""
+        mockResp = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 7697149,
+                    "name": "episodes.dart",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 8566972,
+                    "name": "kratu",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/kratu",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 32,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mockget_json.return_value = mockResp["repos"]
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mockReposUrl:
+            mockReposUrl.return_value = mockResp["repos_url"]
+            self.assertEqual(GithubOrgClient("google").public_repos(),
+                             ["episodes.dart", "kratu"])
+            mockReposUrl.assert_called_once()
+        mockget_json.assert_called_once()
